be7d0204d1d6e0879f4f1b2019d9a8082b248c75
JacobDangCS50 <jacobohdang@gmail.com>
2022-08-22 16:58:42 -0700

    finished lab5

--- src/App.js
+++ src/App.js
@@ -18,35 +18,11 @@ class App extends Component {
     };
   }
 
-  applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
-    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
-    document.documentElement.lang = pickedLanguage;
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
-    this.loadResumeFromPath(resumePath);
-  }
-
-  swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
-  }
 
   componentDidMount = () => {
     this.loadSharedData();
-    this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
-    );
+    var resumePath =`res_primaryLanguage.json`;
+    this.loadResumeFromPath(resumePath);
   }
 
   loadResumeFromPath = (path) => {
@@ -99,7 +75,7 @@ class App extends Component {
         </Routes>
         <Footer 
           sharedBasicInfo={this.state.sharedData.basic_info}
-          applyPickedLanguage={this.applyPickedLanguage} 
+          //applyPickedLanguage={this.applyPickedLanguage} 
         />  
       </Router>
     );
